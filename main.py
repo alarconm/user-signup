293f66ecfbb56e4ddb32890227802d2100699753
Michael Alarcon <malarconii@yahoo.com>
2017-06-11 21:34:47 -0700

    Added validation variables, created welcome landing page

--- main.py
+++ main.py
@@ -8,5 +8,23 @@ def index():
     '''Display home page'''
     return render_template('hello.html', title="User Sign-in App")
 
+@app.route('/', methods=['POST'])
+def validate_user():
+    '''Validate username, password and email address entered'''
+
+    name = request.form['name']
+    password = request.form['password']
+    password_check = request.form['password_check']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = ''
+    password_check_error = ''
+    email_error = ''
+
+    
+
+
+
 
 app.run()
